b46717984c9bf6daaaab1ad1e55f922c1d42f5a1
Rosa <149727369+Rosa-do-Carmo@users.noreply.github.com>
2023-11-30 12:53:37 +0000

    variants

--- data/app.js
+++ data/app.js
@@ -4,16 +4,32 @@ if (window.performance) {
     if (performance.navigation.type == 1) {
         var r = Math.floor(Math.random() * 3);
         var r1 = Math.floor(Math.random() * 3);
+        
     }
 };
-
+var v = localStorage.getItem("v");
 var p = location.pathname.split('/').pop().replace(/.html/gi, '');
-var v = 0;
 console.log(p)
 
 fetch('../story/story.json')
     .then(response => response.json())
     .then(data => {
-
         document.querySelector('#texto').innerHTML = data.Story[p].Part.Variant[v].Version[r].Text
     });
+
+// ------------------------------
+
+function insertAndExecute(id, text) {
+    document.getElementById(id).innerHTML = text;
+    var scripts = Array.prototype.slice.call(document.getElementById(id).getElementsByTagName("script"));
+    for (var i = 0; i < scripts.length; i++) {
+        if (scripts[i].src != "") {
+            var tag = document.createElement("script");
+            tag.src = scripts[i].src;
+            document.getElementsByTagName("head")[0].appendChild(tag);
+        }
+        else {
+            eval(scripts[i].innerHTML);
+        }
+    }
+}
