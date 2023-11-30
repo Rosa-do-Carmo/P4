ea195b4530987227866a4d50339b69f33fc10b3f
Rosa <149727369+Rosa-do-Carmo@users.noreply.github.com>
2023-11-30 23:59:10 +0000

    fixed variants

--- data/buttons.js
+++ data/buttons.js
@@ -1,5 +1,29 @@
-const title = document.querySelectorAll('button');
+function pathA() {
+    var v = 0;
+    localStorage.setItem("v", v);
+};
+function pathB() {
+    var v = 1;
+    localStorage.setItem("v", v);
+};
+function pathC() {
+    var v = 2;
+    localStorage.setItem("v", v);
+};
 
-function alertBtn() {
-    alert('Ayy, it works :D');
+// -----------------
+
+function insertAndExecute(id, text) {
+    document.getElementById(id).innerHTML = text;
+    var scripts = Array.prototype.slice.call(document.getElementById(id).getElementsByTagName("script"));
+    for (var i = 0; i < scripts.length; i++) {
+        if (scripts[i].src != "") {
+            var tag = document.createElement("script");
+            tag.src = scripts[i].src;
+            document.getElementsByTagName("head")[0].appendChild(tag);
+        }
+        else {
+            eval(scripts[i].innerHTML);
+        }
+    }
 }
